3c1a5fd54a17691e317c989b7bda27c962f11961
mpx9411 <olemagnuspalmstierna@gmail.com>
2021-05-30 17:01:39 +0200

    They are playing now!!

--- src/Othello.java
+++ src/Othello.java
@@ -1,23 +1,29 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
-enum Color{
+enum PLAYER {
     BLANK,
-    WHITE, //BOT
-    BLACK, //PLAYER
+    MINIMAX, //BOT
+    MARKOV, //PLAYER
     FALSE
 }
 
 public class Othello implements Comparable {
 
+    double probability = 0.8;
+    int nextToCorner = -20;
+    int corner = 50;
+    int border = 20;
+
+
+
     static Scanner input = new Scanner(System.in);
     static HashMap<Tile, Othello> realBotMoves = new HashMap<>();
     ArrayList<Othello> moves = new ArrayList<>();
     Tile[][] board;
-    int whiteTiles = 0; //REINFORCEMENT PLAYER
-    int blackTiles = 0; //MINIMAX PLAYER
+    int miniTiles = 0; //MINIMAX PLAYER +
+    int markovTiles = 0; //REINFORCEMENT PLAYER -
     int possibleMoves;
     int score = 0;
     int turnCounter = 1;
@@ -25,7 +31,7 @@ public class Othello implements Comparable {
     float reward = 0;
     Tile lastMove;
 
-    int foresight = 5;
+    int foresight = 3;
     float discount = (float) 0.5;
 
     private Othello(Tile[][] board, int depth) {
@@ -36,30 +42,32 @@ public class Othello implements Comparable {
     public static void main(String[] args) throws InterruptedException {
         Othello game = new Othello(new Tile[8][8], 0);
         System.out.println("Welcome to Othello. Enter the X and Y panel you wish to play.");
-        game.setFlatRewards();
         game.run();
     }
 
     private void run() throws InterruptedException {
         constructBoard();
         while (!setOver()) {
+
+
             Thread.sleep(500);
-            printBoard();
             if (turnCounter == 1) {
-                blackMoveStart();
-                turnCounter++;
-            } else if (turnCounter % 2 == 0) {
-                System.out.println("Bot 1 turn");
-                blackMove(Color.BLACK);
+                miniMoveStart();
                 turnCounter++;
             } else if (turnCounter % 2 != 0) {
-                System.out.println("Bot 2 turn");
-                blackMove(Color.WHITE);
+                System.out.println("Mini turn");
+                botMove(PLAYER.MINIMAX);
+                turnCounter++;
+            } else if (turnCounter % 2 == 0) {
+                System.out.println("Markov turn");
+                setRewards(PLAYER.MARKOV);
+                makeOptimalMove(PLAYER.MARKOV);
                 turnCounter++;
             }
+            printBoard();
 
-            System.out.println("Bot 1 score: " + whiteTiles);
-            System.out.println("Bot 2 score: " + blackTiles);
+            System.out.println("Mini score: " + miniTiles);
+            System.out.println("Markov score: " + markovTiles);
 
         }
     }
@@ -71,10 +79,10 @@ public class Othello implements Comparable {
             System.out.print(i + " ");
             for (int j = 0; j < 8; j++) {
                 switch (board[i][j].getColor()) {
-                    case BLACK:
+                    case MARKOV:
                         System.out.print("[-]");
                         break;
-                    case WHITE:
+                    case MINIMAX:
                         System.out.print("[+]");
                         break;
                     case BLANK:
@@ -88,47 +96,47 @@ public class Othello implements Comparable {
     }
 
     private void flipTiles(int y, int x) {               //CHECK IF ANY OCCUPIED TILES ARE BETWEEN TWO TILES OF OTHER COLOR
-        Color tile = board[y][x].getColor();
+        PLAYER tile = board[y][x].getColor();
         Tile toChange;
-        if (checkTile(y + 2, x) == tile && checkTile(y + 1, x) == otherColor(tile))
+        if (checkTile(y + 2, x) == tile && checkTile(y + 1, x) == otherPlayer(tile))
             board[y + 1][x].setColor(tile);
-        if (checkTile(y - 2, x) == tile && checkTile(y - 1, x) == otherColor(tile))
+        if (checkTile(y - 2, x) == tile && checkTile(y - 1, x) == otherPlayer(tile))
             board[y - 1][x].setColor(tile);
-        if (checkTile(y, x + 2) == tile && checkTile(y, x + 1) == otherColor(tile))
+        if (checkTile(y, x + 2) == tile && checkTile(y, x + 1) == otherPlayer(tile))
             board[y][x + 1].setColor(tile);
-        if (checkTile(y, x - 2) == tile && checkTile(y, x - 1) == otherColor(tile))
+        if (checkTile(y, x - 2) == tile && checkTile(y, x - 1) == otherPlayer(tile))
             board[y][x - 1].setColor(tile);
-        if (checkTile(y - 2, x - 2) == tile && checkTile(y - 1, x - 1) == otherColor(tile))
+        if (checkTile(y - 2, x - 2) == tile && checkTile(y - 1, x - 1) == otherPlayer(tile))
             board[y - 1][x - 1].setColor(tile);
-        if (checkTile(y - 2, x + 2) == tile && checkTile(y - 1, x + 1) == otherColor(tile))
+        if (checkTile(y - 2, x + 2) == tile && checkTile(y - 1, x + 1) == otherPlayer(tile))
             board[y - 1][x + 1].setColor(tile);
-        if (checkTile(y + 2, x + 2) == tile && checkTile(y + 1, x + 1) == otherColor(tile))
+        if (checkTile(y + 2, x + 2) == tile && checkTile(y + 1, x + 1) == otherPlayer(tile))
             board[y + 1][x + 1].setColor(tile);
-        if (checkTile(y + 2, x - 2) == tile && checkTile(y + 1, x - 1) == otherColor(tile))
+        if (checkTile(y + 2, x - 2) == tile && checkTile(y + 1, x - 1) == otherPlayer(tile))
             board[y + 1][x - 1].setColor(tile);
 
     }
 
-    private void blackMoveStart() {                        //SINCE START OF GAME IS PERFECTLY SYMMETRICAL, WE CAN RANDOMIZE THE BOT'S FIRST MOVE IF HE BEGINS
+    private void miniMoveStart() {                        //SINCE START OF GAME IS PERFECTLY SYMMETRICAL, WE CAN RANDOMIZE THE BOT'S FIRST MOVE IF HE BEGINS
         double move = Math.floor(Math.random() * 3);
         switch ((int) move) {
             case 0:
-                makeMove(board, 2, 3, Color.WHITE);
+                makeMove(board, 2, 3, PLAYER.MINIMAX);
                 break;
             case 1:
-                makeMove(board, 3, 2, Color.WHITE);
+                makeMove(board, 3, 2, PLAYER.MINIMAX);
                 break;
             case 2:
-                makeMove(board, 4, 5, Color.WHITE);
+                makeMove(board, 4, 5, PLAYER.MINIMAX);
                 break;
             case 3:
-                makeMove(board, 5, 4, Color.WHITE);
+                makeMove(board, 5, 4, PLAYER.MINIMAX);
                 break;
         }
     }
 
     private void manualMove() {
-        Color myColor = Color.BLACK;
+        PLAYER myColor = PLAYER.MARKOV;
         if (hasValidMove(myColor)) {
             char[] move;
             do {
@@ -141,7 +149,7 @@ public class Othello implements Comparable {
         }
     }
 
-    private boolean hasValidMove(Color color) {
+    private boolean hasValidMove(PLAYER color) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (isValidMove(i, j, color)) {
@@ -155,9 +163,9 @@ public class Othello implements Comparable {
     private boolean setOver() {                      //CHECK IF GAME IS OVER
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (isValidMove(i, j, Color.WHITE))
+                if (isValidMove(i, j, PLAYER.MINIMAX))
                     return false;
-                if (isValidMove(i, j, Color.BLACK))
+                if (isValidMove(i, j, PLAYER.MARKOV))
                     return false;
             }
         }
@@ -165,8 +173,7 @@ public class Othello implements Comparable {
     }
 
 
-    private void blackMove(Color color) {                        //EXECUTE BOT MOVE WITH A CALCULATED LIST OF OPTIMAL MOVES
-
+    private void botMove(PLAYER color) {                        //EXECUTE BOT MOVE WITH A CALCULATED LIST OF OPTIMAL MOVES
         miniMax(color);
         Tile move = null;
         for (Tile t : realBotMoves.keySet()) {
@@ -186,7 +193,7 @@ public class Othello implements Comparable {
         }
     }
 
-    private Othello miniMax(Color color) {
+    private Othello miniMax(PLAYER color) {
 
         HashMap<Tile, Othello> possibleMoves = new HashMap<>();             //STORE INSTANCES POSSIBLE MOVES
         ArrayList<Othello> optimalMoves = new ArrayList<>();                //STORE OPTIMAL MOVES
@@ -194,23 +201,31 @@ public class Othello implements Comparable {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (isValidMove(i, j, color)) {
-                    Othello theory = new Othello(board, depth + 1);      //IF A VALID MOVE, CREATE SCENARIO AND INSERT INTO POSSIBLE MOVES
+                    Othello theory = new Othello(new Tile[8][8], depth+1);//IF A VALID MOVE, CREATE SCENARIO AND INSERT INTO POSSIBLE MOVES
+                    for(int k = 0; k < 8; k++) {
+                        for (int l = 0; l < 8; l++) {
+
+                            theory.board[k][l] = new Tile(board[k][l].getY(), board[k][l].getX(), board[k][l].getColor());
+
+                        }//IF A VALID MOVE, CREATE SCENARIO AND INSERT INTO POSSIBLE MOVES
+                    }
                     theory.makeMove(theory.board, i, j, color);
                     possibleMoves.put(theory.lastMove, theory);
+
 
                 }
             }
         }
         this.possibleMoves = possibleMoves.size();
-        if (possibleMoves.isEmpty() || depth > 3) { //IF THERE ARE NO MORE MOVES OR IF WE'VE GONE TOO DEEP, RETURN SCORE
+        if (possibleMoves.isEmpty() || depth > 5) { //IF THERE ARE NO MORE MOVES OR IF WE'VE GONE TOO DEEP, RETURN SCORE
             return this;
         }
         for (Othello t : possibleMoves.values()) {
             if (!t.setOver()) //                     IF WE'RE NOT AT THE END OF THE GAME, CALCULATE THE BEST MOVE FOR THE NEXT TURN, BUT FOR THE OTHER COLOR
-                t.score += t.miniMax(otherColor(color)).getScore();
+                t.score += t.miniMax(otherPlayer(color)).getScore();
             if (optimalMoves.isEmpty())
                 optimalMoves.add(t);
-            else if ((t.getScore() > optimalMoves.get(0).getScore() && color == Color.WHITE) || (t.getScore() < optimalMoves.get(0).getScore() && color == Color.BLACK)) {
+            else if ((t.getScore() > optimalMoves.get(0).getScore() && color == PLAYER.MINIMAX) || (t.getScore() < optimalMoves.get(0).getScore() && color == PLAYER.MARKOV)) {
                 optimalMoves.clear();
                 optimalMoves.add(t);
             } else if (t.getScore() == optimalMoves.get(0).getScore()) {
@@ -239,64 +254,72 @@ public class Othello implements Comparable {
         return this;
     }
 
-    private void setRewards(Color color) {
+    private void setRewards(PLAYER color) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (isValidMove(i, j, color)) {
-                    Othello future = new Othello(board, depth + 1);  //Create a board that simulates a move
+                    Othello future = new Othello(new Tile[8][8], depth+1);      //IF A VALID MOVE, CREATE SCENARIO AND INSERT INTO POSSIBLE MOVES
+                    for(int k = 0; k < 8; k++){
+                        for(int l = 0; l < 8; l++){
+                            future.board[k][l] = new Tile(board[k][l].getY(), board[k][l].getX(), board[k][l].getColor());
+                        }
+                    } //Create a board that simulates a move
                     future.makeMove(future.board, i, j, color);
                     future.setScore();
                     future.reward += future.getScore(); //Add the added score of making the move to the reward
                     future.reward += board[i][j].getFlatReward();
-                    if (depth < foresight) { //Check if we've looked as far into the future as we want
-                        future.setRewards(otherColor(color)); //Assume the other player does a similar calculation
-                        double randomizer = Math.random()*10;
-                        if(randomizer < probability)
-                            future.makeOptimalMove(otherColor(color));
-                        else
-                            future.makeSecondOptimalMove(otherColor(color));
-                        future.setRewards(color);
-                        future.reward += moves.get(0).reward * discount; //The future move is worth it's own accumulated value plus future optimal values times a discount value
-                    }
                     moves.add(future);
+                    if (depth < foresight && future.moves.size() > 0) { //Check if we've looked as far into the future as we want
+                        future.setRewards(otherPlayer(color)); //Assume the other player does a similar calculation
+                        double randomizer = Math.random()*10;
+                            if (randomizer < probability || future.moves.size() < 2)
+                                future.makeOptimalMove(otherPlayer(color));
+                            else
+                                future.makeSecondOptimalMove(otherPlayer(color));
+                            future.setRewards(color);
+                            future.reward += moves.get(0).reward * discount; //The future move is worth it's own accumulated value plus future optimal values times a discount value
+                    }
+
                 }
             }
         }
     }
 
-    private void makeSecondOptimalMove(Color color) {
+    private void makeSecondOptimalMove(PLAYER color) {
         Tile move;
-        if (color.equals(Color.BLACK)) {
+        if (color.equals(PLAYER.MARKOV)) {
             move = moves.get(moves.size() - 2).lastMove;
         } else {
             move = moves.get(1).lastMove;
         }
-        makeMove(board, move.getX(), move.getY(), color);
+        makeMove(board, move.getY(), move.getX(), color);
     }
 
-    private void makeOptimalMove(Color color) {
+    private void makeOptimalMove(PLAYER color) {
         Tile move;
-        if (color.equals(Color.BLACK)) {
-            move = moves.get(moves.size() - 1).lastMove;
-        } else {
+
+        if (color.equals(PLAYER.MARKOV)) {
             move = moves.get(0).lastMove;
+        } else {
+            move = moves.get(moves.size() - 1).lastMove;
         }
-        makeMove(board, move.getX(), move.getY(), color);
+        makeMove(board, move.getY(), move.getX(), color);
+        moves.clear();
     }
 
     private void constructBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                board[i][j] = new Tile(i, j, Color.BLANK);
+                board[i][j] = new Tile(i, j, PLAYER.BLANK);
                 if ((i == 3 && j == 3) || (i == 4 && j == 4))
-                    board[i][j].setColor(Color.BLACK);
+                    board[i][j].setColor(PLAYER.MARKOV);
                 else if ((i == 3 && j == 4) || (i == 4 && j == 3))
-                    board[i][j].setColor(Color.WHITE);
+                    board[i][j].setColor(PLAYER.MINIMAX);
             }
         }
     }
 
-    private void makeMove(Tile[][] board, int y, int x, Color color) {
+    private void makeMove(Tile[][] board, int y, int x, PLAYER color) {
         if (isValidMove(y, x, color)) {
             board[y][x].setColor(color);
             setScore();
@@ -307,58 +330,60 @@ public class Othello implements Comparable {
         }
     }
 
-    private boolean isValidMove(int y, int x, Color color) {
-        if (board[y][x].getColor() == Color.BLANK) {
-            if (checkTile(y - 2, x) == color && checkTile(y - 1, x) == otherColor(color))
+    private boolean isValidMove(int y, int x, PLAYER color) {
+        if (board[y][x].getColor() == PLAYER.BLANK) {
+            if (checkTile(y - 2, x) == color && checkTile(y - 1, x) == otherPlayer(color))
                 return true;
-            if (checkTile(y + 2, x) == color && checkTile(y + 1, x) == otherColor(color))
+            if (checkTile(y + 2, x) == color && checkTile(y + 1, x) == otherPlayer(color))
                 return true;
-            if (checkTile(y, x - 2) == color && checkTile(y, x - 1) == otherColor(color))
+            if (checkTile(y, x - 2) == color && checkTile(y, x - 1) == otherPlayer(color))
                 return true;
-            if (checkTile(y, x + 2) == color && checkTile(y, x + 1) == otherColor(color))
+            if (checkTile(y, x + 2) == color && checkTile(y, x + 1) == otherPlayer(color))
                 return true;
-            if (checkTile(y + 2, x + 2) == color && checkTile(y + 1, x + 1) == otherColor(color))
+            if (checkTile(y + 2, x + 2) == color && checkTile(y + 1, x + 1) == otherPlayer(color))
                 return true;
-            if (checkTile(y - 2, x + 2) == color && checkTile(y - 1, x + 1) == otherColor(color))
+            if (checkTile(y - 2, x + 2) == color && checkTile(y - 1, x + 1) == otherPlayer(color))
                 return true;
-            if (checkTile(y + 2, x - 2) == color && checkTile(y + 1, x - 1) == otherColor(color))
+            if (checkTile(y + 2, x - 2) == color && checkTile(y + 1, x - 1) == otherPlayer(color))
                 return true;
-            if (checkTile(y - 2, x - 2) == color && checkTile(y - 1, x - 1) == otherColor(color))
+            if (checkTile(y - 2, x - 2) == color && checkTile(y - 1, x - 1) == otherPlayer(color))
                 return true;
 
         }
         return false;
     }
 
-    private Color otherColor(Color color) {
-        if (color == Color.BLACK)
-            return Color.WHITE;
-        else if (color == Color.WHITE)
-            return Color.BLACK;
-        return Color.FALSE;
+    private PLAYER otherPlayer(PLAYER color) {
+        if (color == PLAYER.MARKOV)
+            return PLAYER.MINIMAX;
+        else if (color == PLAYER.MINIMAX)
+            return PLAYER.MARKOV;
+        return PLAYER.FALSE;
     }
 
-    private Color checkTile(int y, int x) {
+    private PLAYER checkTile(int y, int x) {
         if (y < 0 || y > 7 || x < 0 || x > 7)
-            return Color.FALSE;
+            return PLAYER.FALSE;
         else
             return board[y][x].getColor();
     }
 
     private void setScore() {
+        miniTiles = 0;
+        markovTiles = 0;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 switch (checkTile(j, i)) {
-                    case WHITE:
-                        whiteTiles++;
+                    case MINIMAX:
+                        miniTiles++;
                         break;
-                    case BLACK:
-                        blackTiles++;
+                    case MARKOV:
+                        markovTiles++;
                         break;
                     case BLANK:
                         break;
                 }
-                score = blackTiles - whiteTiles;
+                score = markovTiles - miniTiles;
 
             }
         }
@@ -377,9 +402,9 @@ public class Othello implements Comparable {
         int x;
         int y;
         int flatReward;
-        Color color;
+        PLAYER color;
 
-        private Tile(int y, int x, Color color) {
+        private Tile(int y, int x, PLAYER color) {
             this.color = color;
             this.y = y;
             this.x = x;
@@ -403,15 +428,15 @@ public class Othello implements Comparable {
             }
         }
 
-        private int getFlatReward(){
+        int getFlatReward(){
             return flatReward;
         }
 
-        private void setColor(Color color) {
+        private void setColor(PLAYER color) {
             this.color = color;
         }
 
-        private Color getColor() {
+        private PLAYER getColor() {
             return color;
         }
 
